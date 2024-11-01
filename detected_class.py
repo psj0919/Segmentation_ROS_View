b3ad11569ec1f2523e0d5b0daa145de2173b3d80
psj0919 <110454977+psj0919@users.noreply.github.com>
2024-11-01 20:36:17 +0900

    Add files via upload

---
+++ detected_class.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+import cv2
+import sys
+
+sys.path.append("/home/parksungjun/vehicle_segmentation-main")
+import torch
+from cv_bridge import CvBridge
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int32
+from Core.demo import demo
+
+Resize_size = 256
+weight_file = "/home/parksungjun/Downloads/fcn_epochs_200_optimizer_adam_lr_0.0001_modelfcn8.pth"
+GPU_ID = '0'
+DEVICE = torch.device('cuda:0')
+network_name = 'FCN8s' 
+num_class = 21
+
+Detected_class = demo(network_name, Resize_size, DEVICE, weight_file, num_class)
+
+class detected_class:
+    def __init__(self):
+        self.input_image = rospy.Subscriber("/fps_controller/image_raw", Image, self.callback)
+        self.result_image = rospy.Publisher("/detected_class/result_image", Image, queue_size=10)
+        self.bridge = CvBridge()
+        print(1)
+    def callback(self, data):
+        try:
+            print(1)
+            cv_input_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            result_image = Detected_class.run(cv_input_image)
+            result_image = self.bridge.cv2_to_imgmsg
+            self.result_image.publish(result_image)
+        except:
+            print("Error")
+
+def main():
+    class_detector = detected_class()
+    rospy.init_node('class_detector', anonymous=True)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("shut down")
+        cv2.destroyWindow()
+
+if __name__=='__main__':
+    main()
