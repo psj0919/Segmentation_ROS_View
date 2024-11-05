916946cb6a02f4c0419ab418a11dba5945d858cb
psj0919 <110454977+psj0919@users.noreply.github.com>
2024-11-05 16:58:54 +0900

    Update image_view.py

--- image_view.py
+++ image_view.py
@@ -4,37 +4,29 @@ import cv2
 import rospy
 from sensor_msgs.msg import Image
 from ringbuffer import CircularBuffer
-from function import imgmsg_to_cv2
 from cv_bridge import CvBridge
 import matplotlib.pyplot as plt
 
 
 class image_view:
     def __init__(self):
-        self.input_image = rospy.Subscriber("/fps_controller/image_raw", Image, self.callback)
-        self.fps_result_iamge_sub = rospy.Subscriber("/detected_class/result_image",Image, self.callback_result)
-        self.buffer = CircularBuffer(50)
-        self.buffer2 = CircularBuffer(50)
+        #self.input_image = rospy.Subscriber("/fps_controller/image_raw", Image, self.callback)
+        self.fps_result_iamge_sub = rospy.Subscriber("/detected_class/result_image",Image, self.callback)
+        self.buffer = CircularBuffer(100)
         self.bridge = CvBridge()
+        self.count = 0
 
     def callback(self, data):
         try: 
             self.cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
             self.buffer.enqueue(self.cv_image)
-            cv2.imshow('Image View', self.buffer.dequeue())
+            self.count += 1
+            print(self.count)
+            cv2.imshow("Image View", self.buffer.dequeue())
             cv2.waitKey(33)
-        except CvBridgeError as e:
-            print(e)
 
-    def callback_result(self, data):
-        try:
-
-            self.cv_image2 = self.bridge.imgmsg_to_cv2(data, 'rgba8')
-            self.buffer2.enqueue(self.cv_image2)
-            cv2.imshow('Result Image View', self.buffer2.dequeue())
-            cv2.waitKey(33)
-        except:
-            print("Result_Image View Error")	
+        except Exception as e: 
+            print(str(e))
 
 
 def main():
