f9f5155b1f7fc4c9e34b9cf2737f3d37e9a69a42
psj0919 <110454977+psj0919@users.noreply.github.com>
2024-11-15 17:45:57 +0900

    Update demo.py

--- vehicle_segmentation-main/Core/demo.py
+++ vehicle_segmentation-main/Core/demo.py
@@ -16,6 +16,12 @@ CLASSES = [
     'yellowLane', 'blueLane', 'constructionGuide', 'trafficDrum',
     'rubberCone', 'trafficSign', 'warningTriangle', 'fence']
 
+color_table = {0: (0, 0, 0), 1: (128, 0, 0), 2: (0, 128, 0), 3: (0, 0, 128), 4: (128, 128, 0),
+               5: (128, 0, 128), 6: (0, 128, 128), 7: (128, 128, 128), 8: (0, 64, 64),
+               9: (64, 64, 64), 10: (0, 0, 192), 11: (192, 0, 192), 12: (0, 192, 192),
+               13: (192, 192, 192), 14: (64, 128, 0), 15: (192, 0, 128), 16: (64, 128, 128),
+               17: (192, 128, 128), 18: (128, 64, 0), 19: (128, 192, 0), 20: (0, 64, 128)}
+
 class demo:
     def __init__(self, network_name, resize_size, org_size_w, org_size_h, device, weight_path, num_class):
         self.network_name = network_name
@@ -25,6 +31,7 @@ class demo:
         self.device = device
         self.weight_path = weight_path
         self.num_class = num_class
+        self.color_table = self.get_color_table(color_table)
         self.model = self.load_network()
         self.load_weight()
 
@@ -48,6 +55,11 @@ class demo:
         resume_state_dict = ckpt['model'].state_dict()
 
         self.model.load_state_dict(resume_state_dict, strict=True)  # load weights
+        
+    def get_color_table(self, table):
+        color_table_tensor = torch.tensor(list(table.values()), device = self.device, dtype=torch.float)
+
+        return color_table_tensor
 
 
     def run(self, data):
@@ -69,13 +81,17 @@ class demo:
 
         # make_segmentation
 
-        s_time = time.time()           
-        segmentation_iamge = self.pred_to_rgb(output[0])
-        e_time = time.time()
-        #print("pred_to_rgb:", 1 / (e_time - s_time))
                 
+        segmentation_iamge = self.pred_to_rgb(output[0], self.color_table)
+
+        s_time = time.time()                
         segmentation_iamge = self.resize_output(segmentation_iamge, self.org_size_w, self.org_size_h)
-        segmentation_iamge = cv2.addWeighted(data, 1, segmentation_iamge, 0.5, 0)
+        e_time = time.time()
+        print("pred_to_rgb:", (e_time - s_time))
+        #segmentation_iamge = cv2.addWeighted(data, 1, segmentation_iamge, 0.5, 0)
+        
+        segmentation_iamge = data + segmentation_iamge
+
       
         return segmentation_iamge
 
@@ -96,7 +112,11 @@ class demo:
     	output = F.interpolate(output, size=(w_size, h_size), mode='bilinear', align_corners=True).squeeze(0)
     	#output = self.matplotlib_outputshow(output)
     	output = output.permute(1, 2, 0)
-    	return output.detach().cpu().numpy().astype(np.uint8)
+    	s_time = time.time()
+    	output = output.detach().cpu().numpy().astype(np.uint8)
+    	e_time = time.time()
+    	#print("output to cpu: {}".format((e_time - s_time)))
+    	return output
         
         
     def matplotlib_imshow(self, img):
@@ -110,41 +130,24 @@ class demo:
         npimg = img.numpy()
         return (np.transpose(npimg, (1, 2, 0))[:, :, ::-1]).astype(np.uint8)
 
-    def pred_to_rgb(self, pred):
+    def pred_to_rgb(self, pred, table):
         assert len(pred.shape) == 3
         #
+
         pred = pred.softmax(dim=0)
         pred = pred.argmax(dim=0)
-        #pred = pred.detach().cpu().numpy()
-
 
         #
+        
         w, h = pred.shape[0], pred.shape[1]
+
         pred_rgb = np.zeros((w, h), dtype=np.uint8)
-        pred_rgb = np.repeat(np.expand_dims(pred_rgb[:,:],axis=-1),3, -1)
-        #
-        color_table = {0: (0, 0, 0), 1: (128, 0, 0), 2: (0, 128, 0), 3: (0, 0, 128), 4: (128, 128, 0),
-                       5: (128, 0, 128), 6: (0, 128, 128), 7: (128, 128, 128), 8: (0, 64, 64),
-                       9: (64, 64, 64), 10: (0, 0, 192), 11: (192, 0, 192), 12: (0, 192, 192),
-                       13: (192, 192, 192), 14: (64, 128, 0), 15: (192, 0, 128), 16: (64, 128, 128),
-                       17: (192, 128, 128), 18: (128, 64, 0), 19: (128, 192, 0), 20: (0, 64, 128)}
-                       
-        #
-        color_table_tensor = torch.tensor(list(color_table.values()), device = self.device, dtype=torch.float)
+
+        pred_rgb = np.repeat(np.expand_dims(pred_rgb[:,:],axis=-1),3, -1)       
+        
         pred_rgb = torch.tensor(pred_rgb).type('torch.FloatTensor')
 
-        #s_time2 = time.time()
-        #for i in range(len(CLASSES)):
-            #pred_rgb[pred == i] = torch.tensor(np.array(color_table[i])).type('torch.FloatTensor')
-        pred_rgb = color_table_tensor[pred]   
-        #e_time2 = time.time()
-        #print("segment_view = {}".format(1 / (e_time2 - s_time2)))
-        
-
+        pred_rgb = table[pred]   
+       
         return pred_rgb
         
-
-        
-
-
-
