9faaf296b4e680b9490e527fd41350ab09a88ab6
psj0919 <110454977+psj0919@users.noreply.github.com>
2024-11-28 21:31:26 +0900

    Update demo.py

--- vehicle_segmentation-main/Core/demo.py
+++ vehicle_segmentation-main/Core/demo.py
@@ -11,6 +11,8 @@ from model.FCN16s import FCN16s
 from model.FCN32s import FCN32s
 import torch.nn.functional as F
 
+import torch.autograd.profiler as profiler
+
 CLASSES = [
     'background', 'vehicle', 'bus', 'truck', 'policeCar', 'ambulance', 'schoolBus', 'otherCar',
     'freespace', 'curb', 'safetyZone', 'roadMark', 'whiteLane',
@@ -22,6 +24,7 @@ color_table = {0: (0, 0, 0), 1: (128, 0, 0), 2: (0, 128, 0), 3: (0, 0, 128), 4: 
                9: (64, 64, 64), 10: (0, 0, 192), 11: (192, 0, 192), 12: (0, 192, 192),
                13: (192, 192, 192), 14: (64, 128, 0), 15: (192, 0, 128), 16: (64, 128, 128),
                17: (192, 128, 128), 18: (128, 64, 0), 19: (128, 192, 0), 20: (0, 64, 128)}
+
 
 class demo:
     def __init__(self, network_name, resize_size, org_size_w, org_size_h, device, weight_path, num_class):
@@ -58,53 +61,34 @@ class demo:
         self.model.load_state_dict(resume_state_dict, strict=True)  # load weights
         
     def get_color_table(self, table):
-        color_table_tensor = torch.tensor(list(table.values()), device = self.device, dtype=torch.float)
+        color_table_tensor = torch.tensor(list(table.values()), device = self.device, dtype=torch.int)
 
         return color_table_tensor
         
     @torch.no_grad()
     def run(self, data):
         # pre processing
+        self.model.eval()
 
-        img = np.array(data, dtype=np.uint8)
-        img = self.transform(img)
-        img = img.unsqueeze(0).to(self.device)
+        img = self.transform(data)
         # model
-        print(img.shape)
+        output = self.model(img)    
 
-        
-        output = self.model(img)
-        print(output.shape)
-
-
-
-
-
-        # make_segmentation
-
-        segmentation_iamge = self.pred_to_rgb(output[0], self.color_table)  
-        #torch.cuda.synchronize()       
-        #segmentation_iamge = self.resize_output(segmentation_iamge, self.org_size_w, self.org_size_h)
-
-      
-        return segmentation_iamge
+        return output
 
 
     def transform(self, img):
-        img = img.astype(np.float32)
-        img = img.transpose(2, 0, 1)
-        img = torch.from_numpy(img).float() / 255.0    # when image is into network -> image pixel value is between 0 and 1
+        img1 = np.array(img, dtype=np.uint8)
+        img2 = img1.astype(np.float32)
+        img3 = img2.transpose(2, 0, 1)
+        img4 = torch.from_numpy(img3).float() / 255.0    # when image is into network -> image pixel value is between 0 and 1
+        img5 = img4.unsqueeze(0).to(self.device)
 
-        return img
+        return img5
         
     def resize_train(self, image, size):
         return F.interpolate(image.unsqueeze(0), size, mode='bilinear', align_corners=True).squeeze(0)
 
-    def resize_output(self, image, img_mask):
-    	#output = img_mask.cpu().numpy().astype(np.uint8)
-    	output = image + output
-    	return output
-        
         
     def matplotlib_imshow(self, img):
         assert len(img.shape) == 3
@@ -117,23 +101,16 @@ class demo:
         npimg = img.numpy()
         return (np.transpose(npimg, (1, 2, 0))[:, :, ::-1]).astype(np.uint8)
 
-    def pred_to_rgb(self, pred, table):
+    def pred_to_rgb(self, pred):
         assert len(pred.shape) == 3
         #
-
-        pred = pred.softmax(dim=0)
-        pred = pred.argmax(dim=0)
-
+        pred = pred.softmax(dim=0).argmax(dim=0)
         #
-        
-        w, h = pred.shape[0], pred.shape[1]
-
-        pred_rgb = np.zeros((w, h), dtype=np.uint8)
-
-        pred_rgb = np.repeat(np.expand_dims(pred_rgb[:,:],axis=-1),3, -1)       
-        
-        pred_rgb = torch.tensor(pred_rgb).type('torch.FloatTensor')
-
-        pred_rgb = table[pred]   
-       
+        pred_rgb = self.color_table[pred.long()]
         return pred_rgb
+        
+
+        
+
+
+
