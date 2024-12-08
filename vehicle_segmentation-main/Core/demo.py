a7f8a23e3e45c676076f6a1207343b6e996b109e
psj0919 <110454977+psj0919@users.noreply.github.com>
2024-12-08 15:59:08 +0900

    Update demo.py

--- vehicle_segmentation-main/Core/demo.py
+++ vehicle_segmentation-main/Core/demo.py
@@ -19,12 +19,6 @@ CLASSES = [
     'yellowLane', 'blueLane', 'constructionGuide', 'trafficDrum',
     'rubberCone', 'trafficSign', 'warningTriangle', 'fence']
 
-color_table = {0: (0, 0, 0), 1: (128, 0, 0), 2: (0, 128, 0), 3: (0, 0, 128), 4: (128, 128, 0),
-               5: (128, 0, 128), 6: (0, 128, 128), 7: (128, 128, 128), 8: (0, 64, 64),
-               9: (64, 64, 64), 10: (0, 0, 192), 11: (192, 0, 192), 12: (0, 192, 192),
-               13: (192, 192, 192), 14: (64, 128, 0), 15: (192, 0, 128), 16: (64, 128, 128),
-               17: (192, 128, 128), 18: (128, 64, 0), 19: (128, 192, 0), 20: (0, 64, 128)}
-
 
 class demo:
     def __init__(self, network_name, resize_size, org_size_w, org_size_h, device, weight_path, num_class):
@@ -35,7 +29,7 @@ class demo:
         self.device = device
         self.weight_path = weight_path
         self.num_class = num_class
-        self.color_table = self.get_color_table(color_table)
+        self.color_table = self.get_color_table()
         self.model = self.load_network()
         self.load_weight()
 
@@ -48,7 +42,7 @@ class demo:
         elif self.network_name == 'FCN32s':
             model = FCN32s(num_class=self.num_class)
 
-        return model.to(self.device).eval()
+        return model.to(self.device)
 
 
     def load_weight(self):
@@ -60,19 +54,31 @@ class demo:
 
         self.model.load_state_dict(resume_state_dict, strict=True)  # load weights
         
-    def get_color_table(self, table):
-        color_table_tensor = torch.tensor(list(table.values()), device = self.device, dtype=torch.int)
+    def get_color_table(self):
+        color_table = {0: (0, 0, 0), 1: (128, 0, 0), 2: (0, 128, 0), 3: (0, 0, 128), 4: (128, 128, 0),
+                   5: (128, 0, 128), 6: (0, 128, 128), 7: (128, 128, 128), 8: (0, 64, 64),
+                   9: (64, 64, 64), 10: (0, 0, 192), 11: (192, 0, 192), 12: (0, 192, 192),
+                   13: (192, 192, 192), 14: (64, 128, 0), 15: (192, 0, 128), 16: (64, 128, 128),
+                   17: (192, 128, 128), 18: (128, 64, 0), 19: (128, 192, 0), 20: (0, 64, 128)}
 
-        return color_table_tensor
         
+        return color_table
+         
     @torch.no_grad()
     def run(self, data):
         # pre processing
         self.model.eval()
-
+      
         img = self.transform(data)
+        s_time = time.time()
+        img = img.to(self.device)
+        e_time = time.time()
+        #print(1 / (e_time - s_time))
         # model
-        output = self.model(img)    
+        
+        output = self.model(img)
+        
+        output = self.pred_to_rgb(output[0], self.color_table)    
 
         return output
 
@@ -81,36 +87,21 @@ class demo:
         img1 = np.array(img, dtype=np.uint8)
         img2 = img1.astype(np.float32)
         img3 = img2.transpose(2, 0, 1)
-        img4 = torch.from_numpy(img3).float() / 255.0    # when image is into network -> image pixel value is between 0 and 1
-        img5 = img4.unsqueeze(0).to(self.device)
+        img4 = torch.from_numpy(img3).float() / 255.0
+        img5 = img4.unsqueeze(0)
 
         return img5
         
-    def resize_train(self, image, size):
-        return F.interpolate(image.unsqueeze(0), size, mode='bilinear', align_corners=True).squeeze(0)
 
-        
-    def matplotlib_imshow(self, img):
-        assert len(img.shape) == 3
-        npimg = img.numpy()
-        return (np.transpose(npimg, (1, 2, 0))[:, :, ::-1] * 255).astype(np.uint8)
-
-
-    def matplotlib_outputshow(self, img):
-        assert len(img.shape) == 3
-        npimg = img.numpy()
-        return (np.transpose(npimg, (1, 2, 0))[:, :, ::-1]).astype(np.uint8)
-
-    def pred_to_rgb(self, pred):
-        assert len(pred.shape) == 3
-        #
+    def pred_to_rgb(self, pred, color_table):
         pred = pred.softmax(dim=0).argmax(dim=0)
-        #
-        pred_rgb = self.color_table[pred.long()]
+       
+        pred = pred.to('cpu', non_blocking=True)
+       
+        pred_rgb = np.zeros_like(pred, dtype=np.uint8)
+        pred_rgb = np.repeat(np.expand_dims(pred_rgb[:, :], axis=-1), 3, -1)
+         
+        for i in range(len(CLASSES)):
+            pred_rgb[pred == i] = np.array(color_table[i])
+            
         return pred_rgb
-        
-
-        
-
-
-
